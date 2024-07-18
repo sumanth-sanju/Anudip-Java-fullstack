5aed0e76052f96b2b8e52aedaa9d41bdbe3b56bb
sumanth-sanju <sumandeva.k@gmail.com>
2024-07-18 23:20:06 +0530

    Add files via upload

---
+++ CheckTriangleorNot.java
@@ -0,0 +1,21 @@
+package labprogram2;
+
+public class CheckTriangleorNot {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			double firstAngle =60;
+			double secondAngle=60;
+			double thirdAngle=60;
+			double totalAngle =firstAngle+secondAngle+thirdAngle;
+			if(totalAngle==180) {
+				System.out.println("It is a Triangle");
+			}
+			else {
+				System.out.println("It is not a Triangle");
+			}
+
+		}
+
+	}
+
