e231a587d313d475517e805e1ca8059adeca4706
sumanth-sanju <sumandeva.k@gmail.com>
2024-07-18 23:20:46 +0530

    Add files via upload

---
+++ FarenheatTocelcius.java
@@ -0,0 +1,15 @@
+package labprogram2;
+
+public class FarenheatTocelcius {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			double fahrenheit =98.6;
+			double celius=(fahrenheit-32)*5/9;
+			System.out.println(celius);
+					
+
+		}
+
+	}
+
