f7553c08371055224505fe58d33a81d48a66bd36
sumanth-sanju <sumandeva.k@gmail.com>
2024-07-18 23:19:08 +0530

    Add files via upload

---
+++ Mycomputer.java
@@ -0,0 +1,22 @@
+package labprograms;
+
+public class Mycomputer {
+	
+		public static void main(String[] args) {
+			// Writing about my computer and its features
+			System.out.println("My computer is Dell .");
+			System.out.println("My computer is containing 16GB RAM and 512GB  ROM.");
+			System.out.println("My computer has backlight keyboard.");
+			System.out.println("My computer has OLED display.");
+			System.out.println("It contains two speakers  loud and clear sound can produce.");
+			System.out.println("It has Intel i5 thirteen generation processor multi tasking and medium works can be performed easily by itself.");
+			System.out.println("It has fingerprint lock");
+			System.out.println("My computer has 720p HD camera webcamp");
+			System.out.println("It has supports 65watt fast charging");
+			System.out.println("My computer battery life for one charge 4 hours");
+			System.out.println("My laptop is lite weight and slim and stylish design");
+
+		}
+
+	}
+
