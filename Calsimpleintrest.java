5aed6584774a23017334da5e7537696f66824492
sumanth-sanju <sumandeva.k@gmail.com>
2024-07-18 23:19:51 +0530

    Add files via upload

---
+++ Calsimpleintrest.java
@@ -0,0 +1,15 @@
+package labprogram2;
+
+public class Calsimpleintrest {
+
+		public static void main(String[] args) {
+			double principle =1000;
+			double rateOfInterest = 5;
+			double time =5;
+			double SimpleInterest = (principle*(rateOfInterest/100)*time);
+			System.out.println(SimpleInterest);
+
+		}
+
+	}
+
