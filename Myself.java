d1fc358e6a7cb441eee1181ff9cbd544ad45b843
sumanth-sanju <sumandeva.k@gmail.com>
2024-07-18 23:18:48 +0530

    Add files via upload

---
+++ Myself.java
@@ -0,0 +1,18 @@
+package labprograms;
+
+public class Myself {
+
+		public static void main(String[] args) {
+			// Writing about myself
+			System.out.println("My name is nagandla sony.");
+			System.out.println("I am from jammikunta which is located in karimnagar district.");
+			System.out.println("I am studying MCA 1st year at Avanthi PG college. ");
+			System.out.println("I have completed my B.sc from sangamithra womens Degree college.");
+			System.out.println("I have a small family");
+			System.out.println("In my family my mother,sister,and me");
+			System.out.println("Currently my sister is jobber");
+			System.out.println("My mother is bussiness women");
+			System.out.println("My Hobbies are Listening music and watching movies ");
+			System.out.println("My favourite movie happy days");
+	}
+	}
